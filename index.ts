6c8da5e867b85eab0dc4c8ad97985ed160b35dfe
Vinay Saravana Ruban <yanivin77@gmail.com>
2024-03-24 13:32:30 +1000

    more errors

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 console.log("running");
 const testFolder = "./contrans/";
-fs.readdir(testFolder, (err, files) => {
-  files.forEach((file) => {
+fs.readdir(testFolder, (err: any, files: any) => {
+  files.forEach((file: any) => {
     console.log("./contrans/" + file)
     fs.rename("./contrans/" + file, "./combatwombatsftc/public/content/" + file, (err) => {
       if (err) {
