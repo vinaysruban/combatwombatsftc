60ddb19af0f578955c18d8d5089cb8ac78d0268b
Vinay Saravana Ruban <yanivin77@gmail.com>
2024-03-26 17:28:14 +1000

    Made system the default

--- combatwombatsftc/src/components/providers.tsx
+++ combatwombatsftc/src/components/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </ThemeProvider>
   );
