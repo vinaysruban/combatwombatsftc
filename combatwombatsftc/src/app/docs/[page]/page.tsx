473ecccc99218b22a7b10adf84fe756dc10e2d21
Vinay Saravana Ruban <yanivin77@gmail.com>
2024-04-06 17:43:27 +1000

    All for nun

--- combatwombatsftc/src/app/docs/[page]/page.tsx
+++ combatwombatsftc/src/app/docs/[page]/page.tsx
@@ -7,7 +7,6 @@ import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
 
-
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 const getPostContent = async (slug: string) => {
@@ -15,7 +14,6 @@ const getPostContent = async (slug: string) => {
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync(file, "utf8");
   const matterResult = matter(content);
-
   return {
     slug,
     content: matterResult.content,
