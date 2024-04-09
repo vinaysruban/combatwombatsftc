17da9b59ac2cdee2e2b19f218421558ae9d39171
Vinay Saravana Ruban <yanivin77@gmail.com>
2024-04-09 11:39:10 +1000

    Fixed colour changes

--- combatwombatsftc/src/app/layout.tsx
+++ combatwombatsftc/src/app/layout.tsx
@@ -15,7 +15,7 @@ const APP_DESCRIPTION =
   "Discover cutting-edge robotics programming and FIRST Tech Challenge (FTC) insights with the Combat Wombats. Access a wealth of pre-written documentation, coding blueprints, and technical guides designed specifically for FTC enthusiasts. Delve into advanced robotics strategies, innovative design concepts, and efficient coding methods. Elevate your skills and excel in FTC competitions with the Combat Wombats' exclusive resources and expertise. FTC #23335's Website";
 
 export const viewport: Viewport = {
-  themeColor: "#8f9853",
+  themeColor: "#ebe2d3",
 };
 
 export const metadata: Metadata = {
